7e1572502d5e258f446e4bf9e6e500898c1600ef
Lev Zlobin <154515515+lengthylyova@users.noreply.github.com>
2024-07-30 16:21:07 +0300

    fix: F401 lint error (#10)

--- app/fastjwtapi/__init__.py
+++ app/fastjwtapi/__init__.py
@@ -1,3 +1,3 @@
-from .core import JWTCore
-from .dependency import JWTDependency
-from .middlewares import JWTAuthenticationMiddleware
+from .core import JWTCore  # noqa: F401
+from .dependency import JWTDependency  # noqa: F401
+from .middlewares import JWTAuthenticationMiddleware  # noqa: F401
