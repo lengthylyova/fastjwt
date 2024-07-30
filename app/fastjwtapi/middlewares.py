5b52005d5f77fbb5bca0176d169ea482de6a769c
Lev Zlobin <154515515+lengthylyova@users.noreply.github.com>
2024-07-30 16:15:04 +0300

    feat: requirements & flake8 linter workflow (#9)
    * feat: requirements & flake8 linter workflow
    
    * fix: actions/checkout version
    
    * feat: Flake8 Lint Badge

--- app/fastjwtapi/middlewares.py
+++ app/fastjwtapi/middlewares.py
@@ -7,4 +7,6 @@ from .core import JWTCore
 class JWTAuthenticationMiddleware(AuthenticationMiddleware):
     def __init__(self, jwt_core: JWTCore, *args, **kwargs):
         self.core = jwt_core
-        super().__init__(*args, **kwargs, backend=JWTAuthenticationBackend(self.core))
+        super().__init__(
+            *args, **kwargs, backend=JWTAuthenticationBackend(self.core)
+        )
